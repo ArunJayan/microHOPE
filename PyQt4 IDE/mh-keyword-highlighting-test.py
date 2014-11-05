476b64a86b0fa9f6d096adb35e2d178c5a29d262
Arun Jayan <arunjayan32@gmail.com>
2014-11-05 16:33:16 +0530

    Microhope IDE Keyword Analysing Python Script

--- PyQt4 IDE/mh-keyword-highlighting-test.py
+++ PyQt4 IDE/mh-keyword-highlighting-test.py
@@ -1,1 +1,34 @@
+#test version 
 
+#This is the Qt Version of MicroHope IDE
+#Here we are using PyQt4 library
+#Author : Arun Jayan
+#Email ID : arunjayan32@gmail.com
+
+__author__ = 'Arun Jayan'
+__email__ = "arunjayan32@gmail.com"
+
+
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+import sys
+
+class MyHighlighter( QSyntaxHighlighter ):
+
+    def __init__( self, parent, theme ):
+      QSyntaxHighlighter.__init__( self, parent )
+      self.parent = parent
+      keyword = QTextCharFormat()
+      ATMEGA32_REG = QTextCharFormat()
+      PREPROCESS_DIR = QTextCharFormat()
+      headerfile = QTextCharFormat()
+      assignmentOperator = QTextCharFormat()
+      delimiter = QTextCharFormat()
+      specialConstant = QTextCharFormat()
+      boolean = QTextCharFormat()
+      number = QTextCharFormat()
+      comment = QTextCharFormat()
+      string = QTextCharFormat()
+      singleQuotedString = QTextCharFormat()
+
+      self.highlightingRules = []
