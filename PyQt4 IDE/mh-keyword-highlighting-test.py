fd3021c53fdca57bf3fc33151c967d32b6fb00ae
Arun Jayan <arunjayan32@gmail.com>
2014-11-05 16:35:15 +0530

    Modified Mh-Keyword Highlighter module

--- PyQt4 IDE/mh-keyword-highlighting-test.py
+++ PyQt4 IDE/mh-keyword-highlighting-test.py
@@ -45,3 +45,107 @@ class MyHighlighter( QSyntaxHighlighter ):
         pattern = QRegExp("\\b" + word + "\\b")
         rule = HighlightingRule( pattern, keyword )
         self.highlightingRules.append( rule )
+      #Preprocessor Directive
+      brush = QBrush( Qt.darkGreen, Qt.SolidPattern )
+      PREPROCESS_DIR.setForeground( brush )
+      PREPROCESS_DIR.setFontWeight( QFont.Bold )
+      pattern = QRegExp("#[^\n]*")
+      rule = HighlightingRule( pattern, PREPROCESS_DIR )
+      self.highlightingRules.append( rule )
+      #header <......>
+      
+      brush = QBrush( Qt.blue, Qt.SolidPattern )
+      headerfile.setForeground( brush ) 
+      headerfile.setFontWeight( QFont.Bold )
+      pattern = QRegExp("<[^\n]*")
+      rule = HighlightingRule( pattern , headerfile )
+      self.highlightingRules.append( rule )
+      
+      # ATMEGA32_REG
+      ATMEGA32_REG.setForeground( brush )
+      keywords = QStringList( [ "DDRC","DDRA","DDRB","DDRD",
+								"PORTA","PORTB","PORTC","PORTD"
+								"ADCSRA","ADEN","ADMUX","REFS0","ADSC"
+								,"ADIF","ADCH","ADCL",
+								"UCSRB","TXEN","RXEN","UBRRH","UBRRL","UCSRC","URSEL","UCSZ1","UCSZ0",
+								"UCSRA","RXC","UDR","UDRE"] )
+      for word in keywords:
+        pattern = QRegExp("\\b" + word + "\\b")
+        rule = HighlightingRule( pattern, ATMEGA32_REG )
+        self.highlightingRules.append( rule )
+
+
+      # assignmentOperator
+      brush = QBrush( Qt.darkGray, Qt.SolidPattern )
+      pattern = QRegExp( "(<){1,2}-" )
+      assignmentOperator.setForeground( brush )
+      assignmentOperator.setFontWeight( QFont.Bold )
+      rule = HighlightingRule( pattern, assignmentOperator )
+      self.highlightingRules.append( rule )
+      
+      # delimiter
+      pattern = QRegExp( "[\)\(]+|[\{\}]+|[][]+" )
+      delimiter.setForeground( brush )
+      delimiter.setFontWeight( QFont.Bold )
+      rule = HighlightingRule( pattern, delimiter )
+      self.highlightingRules.append( rule )
+
+      # specialConstant
+      brush = QBrush( Qt.darkBlue, Qt.SolidPattern )
+      specialConstant.setForeground( brush )
+      keywords = QStringList( [ "Inf", "NA", "NaN", "NULL" ] )
+      for word in keywords:
+        pattern = QRegExp("\\b" + word + "\\b")
+        rule = HighlightingRule( pattern, specialConstant )
+        self.highlightingRules.append( rule )
+
+      # boolean
+      boolean.setForeground( brush )
+      keywords = QStringList( [ "TRUE", "FALSE" ] )
+      for word in keywords:
+        pattern = QRegExp("\\b" + word + "\\b")
+        rule = HighlightingRule( pattern, boolean )
+        self.highlightingRules.append( rule )
+
+      # number
+      pattern = QRegExp( "[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?" )
+      pattern.setMinimal( True )
+      number.setForeground( brush )
+      number.setFontWeight( QFont.Bold )
+      rule = HighlightingRule( pattern, number )
+      self.highlightingRules.append( rule )
+
+      # comment
+      brush = QBrush( Qt.red, Qt.SolidPattern )
+      pattern = QRegExp( "//[^\n]*" )
+      comment.setForeground( brush )
+      rule = HighlightingRule( pattern, comment )
+      self.highlightingRules.append( rule )
+
+      # string
+      brush = QBrush( Qt.magenta, Qt.SolidPattern )
+      pattern = QRegExp( "\".*\"" )
+      pattern.setMinimal( True )
+      string.setForeground( brush )
+      rule = HighlightingRule( pattern, string )
+      self.highlightingRules.append( rule )
+      
+      # singleQuotedString
+      pattern = QRegExp( "\'.*\'" )
+      pattern.setMinimal( True )
+      singleQuotedString.setForeground( brush )
+      rule = HighlightingRule( pattern, singleQuotedString )
+      self.highlightingRules.append( rule )
+
+    def highlightBlock( self, text ):
+      for rule in self.highlightingRules:
+        expression = QRegExp( rule.pattern )
+        index = expression.indexIn( text )
+        while index >= 0:
+          length = expression.matchedLength()
+          self.setFormat( index, length, rule.format )
+          index = text.indexOf( expression, index + length )
+      self.setCurrentBlockState( 0 )
+
+class HighlightingRule():
+
